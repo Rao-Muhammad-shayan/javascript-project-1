b12d4988424462c38c6403d6b39fc37865f8562b
Rao-Muhammad-shayan <areesharao9@gmail.com>
2024-12-17 17:58:58 +0500

    add. javascript

---
+++ script.js
@@ -0,0 +1,73 @@
+// Chapter1
+
+// Answer of Question 1
+// I write script js using alert box
+// alert()
+
+// Answer of Question 2
+// alert( "Error! please enter a valid password" )
+
+// Answer of Question 3
+// alert( "Welcome to JS Land \n Happy coding!" )
+
+// Answer of Question 4
+// alert( "Welcome to JS Land " )
+// alert("Prevent this page  to create additional dialogs")
+
+// Answer of Question 5
+// console.log("Hello.. i can run  through my web browser's console ")
+
+// Chapter2
+
+// Answer of Question 1
+// var username="Ghous Ahmed";
+// alert(username)
+
+// Answer of Question 2
+// var MyName="Rao Muhammad shayan"
+// alert(MyName)
+// Answer of Question 3
+// var message="Hello World"
+// alert(message);
+// Answer of Question 4
+// var a="shayan" ,b="18 years old", c="Certified Mobile Application Developer";
+// alert(a+b+c)
+// Answer of Question 5
+// var a = "pizza";
+// var b = "pizz";
+// var c = "piz";
+// var d = "pi";
+// var e = "p";
+// Answer of Question 6
+// alert(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e);
+// Answer of Question 6
+// var a="shayn123@gmail.com";
+// alert("my email address is"+ " " +a  );
+
+// Answer of Question 7
+// var book="A smarter way to learn js ";
+// alert("Im trying to learn javascript"+ "\n" + "" +book);
+
+// Answer of Question 8
+// document.write("<h1>"+"<em>"+ "yes, I can write content in html using javascript"+"</em>"+"</h1>")
+
+// Answer of Question 9
+// var a=" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(a);
+
+// Chapter3
+// Answer of Question 1
+// var age=19;
+// alert("i'm " +age+ " "+ "year old")
+
+// Answer of Question 2
+// var n = 14; // Number of visits
+// var a = 1; // Initialize visit count
+
+// for (var i = 0; i < n; i++) {
+//     alert("you've visited the site " + a);
+//     a++; // Increment the visit count
+// }
+// Answer of Question 2
+// var BirthYear=2005;
+// document.write("<h1>"+ "My Birth year is"+ " "+ BirthYear+"</h1>"+ " \n"+ "<p>"+"Data type of my declare variable is number" + "</p>")
